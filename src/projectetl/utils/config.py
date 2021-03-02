7b3eeb2427955ffbc485cf9d995bb8ec9d4f9f03
Gabriela Argüelles <gabyarte2@gmail.com>
2021-03-02 21:45:41 +0000

    feat: Add constant path variables to config

--- src/projectetl/utils/config.py
+++ src/projectetl/utils/config.py
@@ -2,22 +2,33 @@ from pathlib import Path
 import pandas as pd
 import io
 
-# loading configurations
-data_dir = (Path(__file__).parent.parent.parent.parent / "data").resolve()
-SURVEY_DATA_PATH = (Path(data_dir) / "survey" / "EHPM 2019.sav").resolve()
+# data loading configuration
+DATA_DIR = (Path(__file__).parent.parent.parent.parent / "data").resolve()
+
+# encft
+ENCFT_DIR = (Path(DATA_DIR) / "survey" / "ENCFT").resolve()
+ENCFT_SURVEY_PATH = (Path(ENCFT_DIR) / "Base ENCFT 20201 - 20204" /
+                     "Base ENCFT 20201 - 20204 - hogar.xlsx").resolve()
+ENCFT_PREVIOUS_SURVEY_PATH = (
+    Path(ENCFT_DIR) / "BD ENCFT 20161-20202.sav").resolve()
+ENCFT_OBJECT_DIR = (Path(ENCFT_DIR) / "object").resolve()
+
+# inflation
+INFLATION_DIR = (Path(DATA_DIR) / 'inflation').resolve()
+INFLATION_OBJECT_DIR = (Path(INFLATION_DIR) / 'object')
+
+# enhogar
+ENHOGAR_DIR = (Path(DATA_DIR) / 'survey' / 'ENHOGAR').resolve()
+
+# S3 profile
 S3_PROFILE_NAME = 'rodrigo'
 S3_BUCKET_NAME = 'covid-response-sv'
 S3_KEY = 'isss/ISSS_19.xlsx'
 
 
-def LOCAL_LOAD_FUNC(path): return pd.read_spss(
-    str(path), convert_categoricals=False)
+def LOCAL_LOAD_FUNC(path):
+    return pd.read_excel(str(path), sheet_name=None)
 
 
 def S3_LOAD_FUNC(obj): return pd.read_excel(
     io.BytesIO(obj['Body'].read()), encoding='utf8')
-
-
-# Global variables
-key_variable_isss = 'nit'
-key_variable_ehpm = 'idboleta'
